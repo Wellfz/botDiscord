4880f75d0d3df07b965ce2703b58db388fef32b8
Wellfz <matheusgomesmini@gmail.com>
2026-03-26 00:27:55 -0300

    voice state update sketch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 load_dotenv()
@@ -17,7 +18,21 @@ async def ola(ctx:commands.Context):
 
     await ctx.reply(f'Olá, {user}!')
 
+@bot.event
+async def on_voice_state_update(member:discord.Member, before, after):
+    user = member.display_name
+    hora = datetime.now().strftime('%H:%M:%S')
+    data = datetime.now().strftime('%d/%m/%Y')
 
+    after = after.channel
+    before = before.channel
+
+    if(before is None and after is not None):
+        print(f'{user} entrou no canal [{after}] as {hora} {data}')
+    elif(before is not None and after is None):
+        print(f'{user} saiu do canal [{before}] as {hora} {data}')
+    elif(before is not None and after is not None):
+        print(f'{user} saiu do canal [{before}] para o canal [{after}] as {hora} {data}')
 
 
 
